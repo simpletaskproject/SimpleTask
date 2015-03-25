2c7de6a1f34177743f53c17de03f8569069efa9d
KacperPucek <kacper.pucek@gmail.com>
2015-03-25 20:54:00 +0100

    Add routes for lists and tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  namespace :api do
+  	resources :lists, only: [:index, :show, :update, :destroy] do
+  		resources :tasks, only: [:index, :update, :destroy]
+  	end
+  end
+
   root to: 'application#angular'
 end
