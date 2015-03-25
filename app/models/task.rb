2feebd058ee73cd7f772cb2c531ede3c8a71516d
KacperPucek <kacper.pucek@gmail.com>
2015-03-25 18:38:15 +0100

    Delete unique title validatiob in task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,5 @@
 class Task < ActiveRecord::Base
 	validates :title, presence: true
-	validates :title, uniqueness: true
 	validates :list_id, presence: true
 	belongs_to :list
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Task do
 	describe 'validations' do
 		it { should validate_presence_of :title }
-		it { should validate_uniqueness_of :title }
 		it { should belong_to :list }
 		it { should validate_presence_of :list_id }
 	end
