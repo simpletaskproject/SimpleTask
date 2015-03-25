a44b8981596d508014e6b93b20b3d9f2769c149a
KacperPucek <kacper.pucek@gmail.com>
2015-03-25 16:34:55 +0100

    Add specs for list model

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -2,5 +2,11 @@ require 'spec_helper'
 
 
 describe List do
-	it { should validate_presence_of :title }
+	describe 'validations' do
+		it { should validate_presence_of :title }
+		it { should validate_uniqueness_of :title }
+		it { should validate_presence_of :user_id	}
+		it { should belong_to :user }
+	end
 end
+
