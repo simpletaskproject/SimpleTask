a515c9fc4cc7c7e03be33e38697d1c344dfe7d35
KacperPucek <kacper.pucek@gmail.com>
2015-03-25 20:59:17 +0100

    Add create route for tasks and lists

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   namespace :api do
-  	resources :lists, only: [:index, :show, :update, :destroy] do
-  		resources :tasks, only: [:index, :update, :destroy]
+  	resources :lists, only: [:index, :show, :create, :update, :destroy] do
+  		resources :tasks, only: [:index, :update, :create, :destroy]
   	end
   end
 
