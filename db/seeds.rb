7e7824bbc9d9bd4ef3ce965ae8defdb7f3f072b4
mskv <podworny.m@gmail.com>
2015-03-25 15:15:55 +0100

    seed example user accounts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(email: 'user1@example.com', password: 'password')
+User.create(email: 'user2@example.com', password: 'password')
