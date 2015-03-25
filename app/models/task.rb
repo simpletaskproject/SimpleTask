97a5f922acd59a02fe8faba04f79a61e64958595
KacperPucek <kacper.pucek@gmail.com>
2015-03-25 17:33:12 +0100

    Generate task model and migrate database

---
+++ app/models/task.rb
@@ -0,0 +1,7 @@
+class Task < ActiveRecord::Base
+	validates :title, presence: true
+	validates :title, uniqueness: true
+	validates :list_id, presence: true
+	belongs_to :list
+	has_many :tasks
+end
