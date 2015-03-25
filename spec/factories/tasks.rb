9a2112657444b01bcef361eb7d6de709c743d144
KacperPucek <kacper.pucek@gmail.com>
2015-03-25 17:35:44 +0100

    Add validation specs for task

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :task do
+    title "MyString"
+description "MyString"
+date "2015-03-25"
+  end
+
+end
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Task do
+	describe 'validations' do
+		it { should validate_presence_of :title }
+		it { should validate_uniqueness_of :title }
+		it { should belong_to :list }
+		it { should validate_presence_of :list_id }
+	end
+end
