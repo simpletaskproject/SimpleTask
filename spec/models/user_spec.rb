da143cb8922c0df5b08f465088ce0b9ba1bce8cb
KacperPucek <kacper.pucek@gmail.com>
2015-03-25 17:48:52 +0100

    Add password presence validation to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ describe User do
 	describe 'validation' do
 		it { should validate_presence_of :email }
 		it { should validate_uniqueness_of :email }
+		it { should validate_presence_of :password }
 		it { should have_many :lists }
 		it { should have_many :tasks }
 	end
