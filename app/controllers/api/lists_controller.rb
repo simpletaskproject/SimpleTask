79dc485e9361587c477d83ef97329bf62b20b410
mskv <podworny.m@gmail.com>
2015-03-25 21:17:40 +0100

    Add methods and before_action to ListsController

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -1,5 +1,29 @@
 class Api::ListsController < ApplicationController
+	before_action :authenticate_user!
 
+	def index
+		render json: current_user.lists
+	end
+
+	def show
+		render json: current_user.lists.friendly.find(params[:id])
+	end
+
+	def create
+		render json: List.create!(list_params)
+	end
+
+	def update
+		list = current_user.lists.friendly.find(params[:id])
+		list.update!(list_params)
+		render json: list
+	end
+	
+	def destroy
+		list = current_user.list.friendly.find(params[:id])
+		list.destroy!
+		head 200
+	end
 
 
 	private
