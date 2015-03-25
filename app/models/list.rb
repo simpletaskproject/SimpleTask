10dead575e8ecddb9899d2f61a65417501c49408
KacperPucek <kacper.pucek@gmail.com>
2015-03-25 18:37:59 +0100

    Delete unique title validation in list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,5 @@
 class List < ActiveRecord::Base
 	validates :title, presence: true
-	validates :title, uniqueness: true
 	validates :user_id, presence: true
 	belongs_to :user
 	has_many :tasks
--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -4,7 +4,6 @@ require 'spec_helper'
 describe List do
 	describe 'validations' do
 		it { should validate_presence_of :title }
-		it { should validate_uniqueness_of :title }
 		it { should validate_presence_of :user_id	}
 		it { should belong_to :user }
 		it { should have_many :tasks }
