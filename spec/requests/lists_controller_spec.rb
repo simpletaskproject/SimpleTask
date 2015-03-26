1d92907be36625b47993c951c35ddc21fff3a107
KacperPucek <kacper.pucek@gmail.com>
2015-03-26 21:30:49 +0100

    Add lists_controlle_spec and a spec for index action

---
+++ spec/requests/lists_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+require 'pry'
+describe Api::ListsController do
+	describe "get request for index" do
+
+		context "as signed in user" do
+
+			let(:user1) { create(:user) }
+			let(:user2)	{ create(:user) }
+			let(:list_attributes) { { title: 'MyTitle' } }
+
+			before do
+        login_as(user1, :scope => :user)
+				5.times { user1.lists.create(list_attributes) }
+      end
+
+      context "as an owner" do
+				it "gets all lists" do
+					get '/api/lists', format: :json
+					json = JSON.parse(response.body)
+					expect(json.length).to eq(5)
+					expect(response.status).to eq(200)
+				end
+			end
+		end
+
+		context "as a not signed user" do
+			it "renders 401 status" do
+				get  '/api/lists', format: :json
+				expect(response.status).to eq(401)
+			end
+		end
+	end
+end
