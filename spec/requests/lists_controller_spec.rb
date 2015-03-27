ca214f0c3ee6d0edcdc41bf9dc194ab285500c87
KacperPucek <kacper.pucek@gmail.com>
2015-03-27 13:55:02 +0100

    Add spec for update in lists controller

--- spec/requests/lists_controller_spec.rb
+++ spec/requests/lists_controller_spec.rb
@@ -73,12 +73,11 @@ describe Api::ListsController do
 
 	describe "POST request for create" do
 		context "as signed in user" do
-			let(:user1) { create(:user) }
-			let(:user2) { create(:user) }
-			let(:valid_params)  { { title: 'Title', user_id: user1.to_param } }
+			let(:user) { create(:user) }
+			let(:valid_params)  { { title: 'Title', user_id: user.to_param } }
 
 			before do
-				login_as(user1, :scope => :user)
+				login_as(user, :scope => :user)
 			end
 
 			context "with valid params" do
@@ -100,6 +99,50 @@ describe Api::ListsController do
 					expect(response.status).to eq(401)
 				end
 				it { expect{ post '/api/lists', list: valid_params }.not_to change{ List.count} }
+			end
+		end
+	end
+
+	describe "PUT update" do
+		context "as signed in user" do
+			let(:user1) { create(:user) }
+			let(:user2) { create(:user) }
+			let(:list1) { create(:list, user: user1) }
+			let(:list2) { create(:list, user: user2) }
+			let(:valid_params) { { title: 'ChangedTitle'} }
+
+			before do
+				login_as(user1, scope: :user)
+			end
+
+			context "as as signed in user" do
+				context "as an owner" do
+					it "updates the list" do
+						put "/api/lists/#{list1.slug}", list: valid_params, format: :json
+						json = JSON.parse(response.body)
+						expect(json['title']).to eq('ChangedTitle')
+						expect(response.status).to eq(200)
+					end
+				end
+				context "as not an owner" do
+					it "doesn't update the list" do
+						put "/api/lists/#{list2.slug}", list: valid_params, format: :json
+						expect(response.status).to eq(401)
+						expect{ put "/api/lists/#{list2.slug}", list: valid_params }.not_to change{ list2.title }
+					end
+				end
+			end
+		end
+
+		context "as not signed user" do
+			let(:user) { create(:user) }
+			let(:list1) { create(:list, user: user) }
+			let(:valid_params) { { title: 'ChangedTitle'} }
+
+			it "doesn't update the list" do
+				put "/api/lists/#{list1.slug}", list: valid_params, format: :json
+				expect(response.status).to eq(401)
+				expect{ put "/api/lists/#{list1.slug}", list: valid_params }.not_to change{ list1.title }
 			end
 		end
 	end
