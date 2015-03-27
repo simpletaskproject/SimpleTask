fcc636ec651209ada2326ef10ff62c4e6470cf89
KacperPucek <kacper.pucek@gmail.com>
2015-03-27 13:12:57 +0100

    Add spec for create in lists controller

--- spec/requests/lists_controller_spec.rb
+++ spec/requests/lists_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 require 'pry'
 describe Api::ListsController do
-	describe "get request for index" do
+	describe "GET request for index" do
 
 		context "as signed in user" do
 
@@ -32,7 +32,7 @@ describe Api::ListsController do
 		end
 	end
 
-	describe "get request for show" do
+	describe "GET request for show" do
 
 		context "as signed in user" do
 
@@ -70,5 +70,40 @@ describe Api::ListsController do
 			end
 		end
 	end
+
+	describe "POST request for create" do
+		context "as signed in user" do
+			let(:user1) { create(:user) }
+			let(:user2) { create(:user) }
+			let(:valid_params)  { { title: 'Title', user_id: user1.to_param } }
+
+			before do
+				login_as(user1, :scope => :user)
+			end
+
+			context "with valid params" do
+				it "creates new list" do
+					post '/api/lists', list: valid_params, format: :json
+					json = JSON.parse(response.body)
+					expect(json['title']).to eq('Title')
+					expect(response.status).to eq(200)
+					expect{ post '/api/lists', list: valid_params }.to change{ List.count }.by(1)
+				end
+			end
+		end
+
+		context "as not signed user" do
+			context "with valid params" do
+				let(:valid_params) { { title: 'Title', user_id: 'mail@gmail' } }
+				it "renders unauthorized" do
+					post '/api/lists', list: valid_params, format: :json
+					expect(response.status).to eq(401)
+				end
+				it { expect{ post '/api/lists', list: valid_params }.not_to change{ List.count} }
+			end
+		end
+	end
+
+
 
 end
