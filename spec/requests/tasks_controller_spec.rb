437858c5498a7599b32a782a5cadf73e69927d14
KacperPucek <kacper.pucek@gmail.com>
2015-03-28 15:27:28 +0100

    Add create spec for tasks controller

--- spec/requests/tasks_controller_spec.rb
+++ spec/requests/tasks_controller_spec.rb
@@ -5,6 +5,7 @@ describe Api::TasksController do
 	let(:user) { create(:user) }
 	let(:list) { create(:list, user: user) }
 	let(:task) { create(:task, list: list) }
+	let(:valid_attributes) { { title: 'Title', list_id: list.to_param } }
 
 	describe "GET reuqest for index" do
 		context "as signed in user" do
@@ -31,4 +32,27 @@ describe Api::TasksController do
 			end
 		end
 	end
+
+	describe "POST request for create" do
+		context "as signed in user" do
+			before do
+				login_as(user, scope: :user)
+			end
+
+			it "creates new task" do
+				expect{ post "/api/lists/#{list.slug}/tasks", task: valid_attributes, format: :json}.to change{ Task.count }.by(1)
+				json = JSON.parse(response.body)
+				expect(json['title']).to eq('Title')
+				expect(response.status).to eq(200)
+			end
+		end
+	end
+
+		context "as not signed user" do
+			it "doesn't create new task" do
+				expect{ post "/api/lists/#{list.slug}/tasks", task: valid_attributes, format: :json}.not_to change{ Task.count }
+				expect(response.status).to eq(401)
+			end
+		end
+
 end
