58fdc454a2b822d0520b36008aed050caea22803
KacperPucek <kacper.pucek@gmail.com>
2015-03-28 13:46:33 +0100

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
 
   namespace :api do
   	resources :lists, only: [:index, :show, :create, :update, :destroy] do
-  		resources :tasks, only: [:index, :update, :create, :destroy]
+  		resources :tasks, only: [:update, :create, :destroy]
   	end
+  	get '/tasks', to: 'tasks#index'
   end
 
   root to: 'application#angular'
