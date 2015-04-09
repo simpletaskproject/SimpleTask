e89df7d16af39c8f9727f31775a594e157b2fca2
mskv <podworny.m@gmail.com>
2015-04-09 23:18:02 +0200

    Add accordion to tasks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,6 @@
 //= require angular-devise
 //= require angular-rails-templates
 //= require angular-ui-router
-//= require angular-ui-bootstrap
+//= require angular-ui-bootstrap-tpls
 //= require angular-animate
 //= require_tree .
