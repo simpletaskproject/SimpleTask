d44485c5ef90fd1e0a443b2a4aeb1dce3fac3bd1
KacperPucek <kacper.pucek@gmail.com>
2015-04-11 12:44:56 +0200

    Add from_omniauth class method to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,13 @@ class User < ActiveRecord::Base
   has_many :tasks, through: :lists
 
   friendly_id :email
+
+   def self.from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+      end
+  end
 end
