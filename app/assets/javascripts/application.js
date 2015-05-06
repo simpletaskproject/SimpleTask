245718e9aad48d02fd0d38d726011b37ed8e7c54
mskv <podworny.m@gmail.com>
2015-05-07 00:02:40 +0200

    Replace angular UI gem with proper official distribution

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,6 @@
 //= require angular-devise
 //= require angular-rails-templates
 //= require angular-ui-router
-//= require angular-ui-bootstrap-tpls
+//= require angular-bootstrap
 //= require angular-animate
 //= require_tree .
